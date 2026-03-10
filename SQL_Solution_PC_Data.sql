df8cab66925c2b7fccd4da57cdc435d16c3b537a
Tamara <tamaragovindsamy@gmail.com>
2026-03-11 01:35:46 +0200

    Pushing to Github

--- SQL_Solution_PC_Data.sql
+++ SQL_Solution_PC_Data.sql
@@ -16,9 +16,9 @@ FROM [PC_Sales].[dbo].[Laptop_Sales]
 group by Continent
 
 -- 3. Count the number of sales per Country or State.
-SELECT COUNT (*) as NumberOfSales, [Country or State]
+SELECT COUNT (*) as NumberOfSales, [Country_or_State] 
 FROM [PC_Sales].[dbo].[Laptop_Sales]
-group by [Country or State]
+group by [Country_or_State]
 
 
 -- 4. List all distinct Shop Name values.
@@ -63,9 +63,17 @@ Select Sum(Sale_Price - Cost_Price) As total_Profit
 FROM [PC_Sales].[dbo].[Laptop_Sales]
 
 -- 13. Find the average Discount Amount.
-SELECT AVG(
+SELECT AVG([Discount_Amount])
+FROM [PC_Sales].[dbo].[Laptop_Sales]
+
+
 -- 14. Calculate total Finance Amount issued.
+SELECT Sum(CAST([Finance_Amount] as INT)) As Total_Amount_issued
+FROM [PC_Sales].[dbo].[Laptop_Sales]
+
+
 -- 15. Find total revenue per PC Make.
+SELECT 
 -- 16. Find average Sale Price per Storage Type.
 -- 17. Calculate total revenue per Shop Name.
 -- 18. Calculate total revenue per Sales Person Name.
@@ -75,13 +83,65 @@ SELECT AVG(
 -- ADVANCED QUESTIONS
 
 -- 21. Calculate profit per Shop Name.
+       select [Shop_Name],
+       sum([Sale_Price]-[Cost_Price]) as Profit
+       FROM [PC_Sales].[dbo].[Laptop_Sales]
+       group by [Shop_Name]
+
+
 -- 22. Calculate profit margin per sale ((Sale Price - Cost Price) / Sale Price).
+       select sum(CAST((Sale_Price - Cost_Price) / (Sale_Price) as INT)) as Profit
+       FROM [PC_Sales].[dbo].[Laptop_Sales]
+
+
 -- 23. Determine which Continent has the highest total revenue.
+Select TOP 1 Continent,sum(Sale_Price) AS total_revenue
+FROM [PC_Sales].[dbo].[Laptop_Sales]
+GROUP BY Continent
+ORDER BY sum(Sale_Price) DESC
+
+
 -- 24. Calculate average Sale Price per RAM size.
+SELECT RAM,  AVG(Sale_Price) as AVG_Sale_Price
+FROM [PC_Sales].[dbo].[Laptop_Sales]
+GROUP BY RAM
+ORDER BY AVG_Sale_Price DESC
+
+
 -- 25. Find the PC Model with the highest Sale Price.
+SELECT TOP 1 [PC_MODEL], [Sale_Price]
+FROM [PC_Sales].[dbo].[Laptop_Sales]
+ORDER BY Sale_Price DESC
+
 -- 26. Calculate the average number of days between Purchase Date and Ship Date.
+SELECT AVG(DATEDIFF(DAY, 
+Try_Cast(Purchase_Date AS datetime), 
+Try_Cast(Ship_Date AS datetime))) AS Days_between_PD_SD
+FROM [PC_Sales].[dbo].[Laptop_Sales]
+WHERE Purchase_Date is not Null and Ship_Date is not Null
+
 -- 27. Determine which Sales Person Department generates the highest revenue.
+SELECT TOP 1 [Sales_Person_Department], sum(Sale_Price) as Revenue
+FROM [PC_Sales].[dbo].[Laptop_Sales]
+GROUP BY Sales_Person_Department
+ORDER BY sum(Sale_Price) DESC
+
 -- 28. Calculate total revenue per Storage Capacity.
+SELECT Storage_Capacity, sum(Sale_Price) AS total_revenue
+FROM [PC_Sales].[dbo].[Laptop_Sales]
+Group by Storage_Capacity
+
 -- 29. Identify sales where Sale Price is lower than PC Market Price.
+SELECT Sale_Price, PC_Market_Price 
+FROM [PC_Sales].[dbo].[Laptop_Sales]
+WHERE Sale_Price < PC_Market_Price
+
 -- 30. Rank Sales Person Name by Total Sales per Employee using a window function.
+SELECT Sales_Person_Name, sum(Sale_Price) AS  Total_Sales_Person_Employee ,
+--ROW_NUMBER () OVER(ORDER BY Sum(Sale_Price)) AS Sale_row_number,
+RANK () OVER(ORDER BY SUM(Sale_Price)) AS Overall_rank
+FROM [PC_Sales].[dbo].[Laptop_Sales]
+GROUP BY Sales_Person_Name
+
+
 
